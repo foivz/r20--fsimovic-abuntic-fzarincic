b1fabc5550894bd3b62a26e34b5f4bdb0b96562c
filipzarincic <fzarincic@foi.hr>
2020-06-16 17:06:16 +0200

    Dodana Forma za izvještaje
    Dodana Forma za izvještaje
    Dodan jedan izvještaj o svim zaposlenicima
    TO DO:
    Povezati izvještaj sa bazom i klik na formu izvještaj

---
+++ Software/PresentationLayer/DocumentsForms/FormIzvještaji.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PresentationLayer.DocumentsForms
+{
+    partial class FormIzvještaji
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSviZaposlenici = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSviZaposlenici
+            // 
+            this.btnSviZaposlenici.Location = new System.Drawing.Point(38, 73);
+            this.btnSviZaposlenici.Name = "btnSviZaposlenici";
+            this.btnSviZaposlenici.Size = new System.Drawing.Size(97, 26);
+            this.btnSviZaposlenici.TabIndex = 0;
+            this.btnSviZaposlenici.Text = "Svi zaposlenici";
+            this.btnSviZaposlenici.UseVisualStyleBackColor = true;
+            // 
+            // FormIzvještaji
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 326);
+            this.Controls.Add(this.btnSviZaposlenici);
+            this.Name = "FormIzvještaji";
+            this.Text = "Izvještaji";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSviZaposlenici;
+    }
+}
---
+++ Software/PresentationLayer/ReportForms/FormSviZaposleniciReport.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.ReportForms
+{
+    public partial class FormSviZaposleniciReport : Form
+    {
+        public FormSviZaposleniciReport()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSviZaposleniciReport_Load(object sender, EventArgs e)
+        {
+            //using(var context = new ClubbingPayDbContext)
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
