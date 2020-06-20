5b84c088c583ae525e8d17c26b2a78eee60d6460
abuntic <abuntic238@gmail.com>
2020-06-20 13:55:02 +0200

    Završen UnitOfWork
    Dodani repozitoriji i interfacei za narudzbenicu i stavku narudzbenice te završen unitofwork

---
+++ Software/BusinessLayer/Repositories/NarudzbenicaRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Services;
+using DatabaseLayer.Interfaces;
+using DatabaseLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Repositories
+{
+    public class NarudzbenicaRepository : GenericRepository<Narudzbenica>, INarudzbenica
+    {
+        private ClubbingPayDbContext _dbContext { get; set; }
+
+        public NarudzbenicaRepository(ClubbingPayDbContext appDbContext)
+            : base(appDbContext)
+        {
+            _dbContext = _appDbContext;
+        }
+    }
+}
